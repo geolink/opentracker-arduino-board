ec6cb5fd4a7bafa4991bca6278520b62cfa9945c
ppescher <p.messina@robotechsrl.com>
2016-05-13 12:57:45 +0200

    fixed definitions of board generated voltages

--- sam/1.0.3/variants/geolink_opentracker/variant.h
+++ sam/1.0.3/variants/geolink_opentracker/variant.h
@@ -237,8 +237,8 @@ static const uint8_t CANTX = 33;
 #define PIN_C_REBOOT    4
 #define PIN_RING_GSM    22
 #define PIN_GSM_VDD_EXT 24
-#define ANALOG_VREF     3.400f
-#define MODEM_VBAT      4.133f
+#define ANALOG_VREF     3.40
+#define MODEM_VBAT      4.13
 #else
 // HW Revision 2.4
 // dropped "REBOOT" pin and "GSM VDD" (unused)
@@ -247,8 +247,8 @@ static const uint8_t CANTX = 33;
 #define PIN_RING_GSM    24
 #define PIN_C_IN1_PD    38
 #define PIN_C_IN2_PD    39
-#define ANALOG_VREF     3.307f
-#define MODEM_VBAT      4.360f
+#define ANALOG_VREF     3.31
+#define MODEM_VBAT      4.36
 #endif
 
 #define AIN_S_INLEVEL   49
